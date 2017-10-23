0ef0930faa8156033a1412deccb49315266cba64
Student <Student@10.2.5.3>
2017-10-23 17:18:48 +0200

    Zadanie 1

---
+++ src/Zadanie1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Zadanie1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+		double c = reader.nextDouble();
+		double f=1.8*c+32.0; 
+		System.out.println(f);
+
+	}
+
+}

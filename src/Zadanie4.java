4db87676f612bd5d60f63b9b9565ab9c83858a55
Student <Student@10.2.5.3>
2017-10-23 18:11:29 +0200

    Zadanie 4

---
+++ src/Zadanie4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Zadanie4 {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Podaj swój dochód");
+		double d = reader.nextDouble();
+		if (d<85528) {
+			
+		}
+	}
+
+}

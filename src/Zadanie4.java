fb2edd9a0f67ad17769efe0f0feaa0414f9cbc9e
danoixe <33001136+danoixe@users.noreply.github.com>
2017-10-29 11:04:16 +0100

    Add files via upload
    Zadanie 4

--- src/Zadanie4.java
+++ src/Zadanie4.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
-public class Zadanie4 {
+public class Zadanie4{
 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 		System.out.println("Podaj swój dochód");
 		double d = reader.nextDouble();
+		double p;
 		if (d<85528) {
-			
+			p = 0.18*d- 556.02;
 		}
+		else {
+			p = 14839.02 + 0.32*(85528-d);
+		}
+		System.out.println("Podatek wynosi "+p+"z³");
 	}
 
 }
